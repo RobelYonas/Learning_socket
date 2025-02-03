725264bbb342f03368f4604fda5205e9f9a2a7e6
Robel Yonas <robel4872@gmail.com>
2025-02-03 14:40:30 +0100

    Update client and server to use localhost and port 5060

--- client.py
+++ client.py
@@ -1,7 +1,15 @@
 import socket
 
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client_socket.connect(('127.0.0.1', 12345))
+client_socket.connect(('localhost', 5060))
 
 msg = client_socket.recv(1024).decode()
-print(f"Server says: {message}")
+print(f"Server says: {msg}")
+
+name = input("what is your name")
+client_socket.send(name.encode())
+
+response = client_socket.recv(1024).decode()
+print(f"server sent: {response}")
+
+client_socket.close()
--- server.py
+++ server.py
@@ -2,7 +2,7 @@ import socket
 
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-server_socket.bind(('127.0.0.1', 12345))
+server_socket.bind(('localhost', 5060))
 
 server_socket.listen(5)
 print("Server is listening ....")
@@ -10,10 +10,12 @@ print("Server is listening ....")
 client_socket, client_address = server_socket.accept()
 print(f"Connection established with {client_address}")
 
-client_socket.send("Hi From server")
+client_socket.send(b"What is your name")
+name = client_socket.recv(1024).decode()
 
+client_socket.send(str.encode(f"Hi {name}"))
 msg = client_socket.recv(1024).decode()
-print(f"Client says: {msg}")
+
 
 client_socket.close()
 server_socket.close()
